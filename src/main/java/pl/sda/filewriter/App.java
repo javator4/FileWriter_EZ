559b158bb676cddd3d6a1e400e0dc7499d9867f1
Emilia Zawisza <emilia.zawisza@wp.pl>
2019-08-03 13:23:50 +0200

    klasy wewnętrzne

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -1,13 +1,33 @@
 package pl.sda.filewriter;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    static class Task1 implements Runnable{ //wewnętrzna klasa
+
+        @Override
+        public void run() {
+            for(int i =0; i<1000; i++){
+                System.out.println("watek 1: " + i);
+            }
+
+        }
+    }
+
+    static class Task2 implements Runnable{
+
+        @Override
+        public void run() {
+            for(int j =0; j<1000; j++){
+                System.out.println("watek 2: " + j);
+            }
+
+        }
+    }
+
+    public static void main( String[] args ){
+
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
     }
 }
