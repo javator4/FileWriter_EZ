af333f0e9b6a76d0d5ba7f6b4601f99e4a8df0a5
Emilia Zawisza <emilia.zawisza@wp.pl>
2019-08-03 13:31:04 +0200

    uśpienie wątku 1 na 500 mms

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -3,39 +3,44 @@ package pl.sda.filewriter;
 
 import java.util.TreeMap;
 
-public class App
-{
-    static class Task1 implements Runnable{ //wewnętrzna klasa
+public class App {
+    static class Task1 implements Runnable { //wewnętrzna klasa
 
         @Override
         public void run() {
-            for(int i =0; i<1000; i++){
+            for (int i = 0; i < 1000; i++) {
                 System.out.println("watek 1: " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (
+                        InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
-
         }
-    }
 
-    static class Task2 implements Runnable{
+        static class Task2 implements Runnable {
 
-        @Override
-        public void run() {
-            for(int j =0; j<1000; j++){
-                System.out.println("watek 2: " + j);
+            @Override
+            public void run() {
+                for (int j = 0; j < 1000; j++) {
+                    System.out.println("watek 2: " + j);
+                }
+
             }
-
         }
-    }
 
-    public static void main( String[] args ){
+        public static void main(String[] args) {
 
-        Task1 task1 = new Task1();
-        Task2 task2 = new Task2();
+            Task1 task1 = new Task1();
+            Task2 task2 = new Task2();
 
-        Thread thread1 = new Thread(task1);
-        Thread thread2 = new Thread(task2);
+            Thread thread1 = new Thread(task1);
+            Thread thread2 = new Thread(task2);
 
-        thread1.start(); // uruchomienie wątku thread1
-        thread2.start();
+            thread1.start(); // uruchomienie wątku thread1
+            thread2.start();
+        }
     }
 }
